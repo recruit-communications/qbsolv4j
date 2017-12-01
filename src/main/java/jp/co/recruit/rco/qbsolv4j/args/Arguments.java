8d33d8c68b87188180abcfa03bf1b4cf2dc37597
kenkoooo <kenkou.n@gmail.com>
2017-12-01 20:47:29 +0900

    Add arguments class

---
+++ src/main/java/jp/co/recruit/rco/qbsolv4j/args/Arguments.java
@@ -0,0 +1,27 @@
+package jp.co.recruit.rco.qbsolv4j.args;
+
+public class Arguments {
+
+
+  final String inputFilepath;
+  final String outputFilepath;
+  final boolean findMaximum;
+  final int subMatrixSize;
+  final int repeats;
+  final long timeoutSeconds;
+  final double targetValue;
+  final int tabuListLength;
+
+  public Arguments(String inputFilepath, String outputFilepath,
+      boolean findMaximum, int subMatrixSize, int repeats, long timeoutSeconds,
+      double targetValue, int tabuListLength) {
+    this.inputFilepath = inputFilepath;
+    this.outputFilepath = outputFilepath;
+    this.findMaximum = findMaximum;
+    this.subMatrixSize = subMatrixSize;
+    this.repeats = repeats;
+    this.timeoutSeconds = timeoutSeconds;
+    this.targetValue = targetValue;
+    this.tabuListLength = tabuListLength;
+  }
+}
